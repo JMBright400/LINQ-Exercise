3092d62f3f552879e48cacbb3fe7f5485f4a12af
John Bright <jmbright400@gmail.com>
2023-10-17 11:32:44 -0500

    video games are in order.

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -2,9 +2,25 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>
+            {
+               "The Legend of Zelda Ocarina of Time",
+               "The Legend of Zelda Majora's Mask",
+               "The Elder Scrolls V: Skyrim",
+               "Super Castlevania IV",
+               "Demon's Souls",
+               "Elden Ring",
+               "Dark Souls",
+            };
+
+            var orderGames = videoGames.OrderBy(game => game.Length);
+
+            foreach (var game in orderGames) 
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
